360e6e110ea2edd866b4437620b1b5e6e0211031
Alexby03 <alexanderby03@gmail.com>
2025-11-19 15:39:03 +0100

    Built project, initial Commit.

---
+++ app/src/main/java/com/example/weatherapp/ui/screens/WeatherScreen.kt
@@ -0,0 +1,17 @@
+package com.example.weatherapp.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun WeatherScreen(
+
+) {
+
+}
+
+@Preview
+@Composable
+fun WeatherScreenPreview() {
+    WeatherScreen()
+}
